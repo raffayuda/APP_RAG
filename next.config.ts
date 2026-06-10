3c567112143c1b1b225e75cb6f2e70fe7f8b7e4f
raffayuda <rafffa0206@gmail.com>
2026-06-10 18:14:19 +0700

    chore: configure Next.js to support server-side execution of transformer and ONNX modules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@xenova/transformers", "onnxruntime-node"],
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/onnxruntime-node/**/*'],
+  },
 };
 
 export default nextConfig;
